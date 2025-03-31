0698dfa952454b2eb67f54a5869b35b41f8de9d4
Jhoan Borges <jhoan.borges@hexagun.mx>
2025-03-30 20:45:57 -0600

    Stable

--- resources/views/filament/pages/demo-with-react.blade.php
+++ resources/views/filament/pages/demo-with-react.blade.php
@@ -1,13 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex flex-col min-h-screen items-center justify-center">
-        <h1 class="text-2xl font-bold mb-4">
-            MingleJS Demo
-        </h1>
-
- 
-        <div class="mt-20 bg-white shadow rounded-lg">
+    <div class="flex flex-col min-h-screen items-center justify-center"> 
+        <div class="mt-0 mb-0 bg-white shadow rounded-lg">
             @livewire(\App\Livewire\PosComponent::class)
         </div>
     </div>
